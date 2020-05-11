5c7411e98941ca152a50d41e54058c37bfd219f0
lrqrachel <56091536+lrqrachel@users.noreply.github.com>
2020-05-11 22:57:23 +0800

    Update data.py

--- dataset1/data.py
+++ dataset1/data.py
@@ -62,7 +62,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
         img,mask = adjustData(img,mask,flag_multi_class,num_class)
         yield (img,mask)
 
-def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
+def testGenerator(test_path,num_image = 2,target_size = (256,256),flag_multi_class = False,as_gray = True):
     for i in range(num_image):
         img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
         img = img / 255
